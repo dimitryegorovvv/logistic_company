6bd2f203cb49de1d2294554187a7b34df150a1c5
Dmitry Egorov <smart260902@gmail.com>
2023-10-31 13:17:40 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+	$('#pagepiling').pagepiling({
+        sectionsColor: ['#42313A', '#1e2a3a', '#121824', '#e77225', '#1e2a3a', '#f9e59e'],
+        loopBottom: true,
+        loopTop: true,
+    });
+});
