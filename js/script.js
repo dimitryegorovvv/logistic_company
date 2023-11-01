c63d42fb4cf79a08b8a4de3cd1aa21045643daab
dimitryegorovvv <132154036+dimitryegorovvv@users.noreply.github.com>
2023-11-01 21:46:01 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	$('#pagepiling').pagepiling({
-        sectionsColor: ['#42313A', '#1e2a3a', '#121824', '#e77225', '#1e2a3a', '#f9e59e'],
+        sectionsColor: ['#1e2a3a', '#1e2a3a', '#121824', '#e77225', '#1e2a3a', '#f9e59e'],
         loopBottom: true,
         loopTop: true,
     });
